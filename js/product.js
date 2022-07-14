ed846e1ccfd4f2e36f57f5adf0e1a3029addca6b
snow <snow61723@gmail.com>
2022-07-14 17:46:32 +0800

    0714

--- js/product.js
+++ js/product.js
@@ -1,7 +1,8 @@
-function proMenuEff(){
+function proMenuEff(page,item){
     $('#proMenu ul ul').hide();
         $('#proMenu>ul>li>a').click(function(){
             $(this).next().stop().slideToggle();
     });
-
+    $('#proMenu ul ul').eq(page).slideDown()
+                       .children('li').eq(item).children('a').addClass('nowPage');
 }
